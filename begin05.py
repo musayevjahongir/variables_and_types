0856539cd389952ad9fc38635d9b9ec1795657db
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-20 10:54:04 +0500

    kod yozdim

--- begin05.py
+++ begin05.py
@@ -1,7 +1,4 @@
-# Assign 7 (int value) to 'a' variable;
-
-# Assign 3 (int value) to 'b' variable;
-
-# Assign 5 (int value) to 'c' variable;
-
-# Print the sum of three variables 'a', 'b', 'c';
+a=7
+b=3
+c=5
+print(a+b+c)
