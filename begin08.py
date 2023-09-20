f5f60c9eb8a3b9d69cc71971e9a3a7a91f0fef0d
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-20 11:04:54 +0500

    kod yozdim

--- begin08.py
+++ begin08.py
@@ -1,7 +1,4 @@
-# Assign 5 (int value) to 'a' variable;
-
-# Assign 4 (int value) to 'b' variable;
-
-# Assign 8 (int value) to 'c' variable;
-
-# Print the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin08';
+a=5
+b=4
+c=8
+print (a*b/c)
