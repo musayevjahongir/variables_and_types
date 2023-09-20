6bb2c9e12d90f1ce163fd399dbb68ba4afec6b88
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-20 11:08:04 +0500

    kod yozdim

--- begin09.py
+++ begin09.py
@@ -1,11 +1,6 @@
-# Assign 8 (int value) to 'a' variable;
-
-# Assign 3 (int value) to 'b' variable;
-
-# Assign 2 (int value) to 'c' variable;
-
-# Assign 4 (int value) to 'd' variable;
-
-# Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin09' to variable 'k';
-
-# # Print the variable 'k'.
+a=8
+b=3
+c=2
+d=4
+k=a+b-c*d
+print (k)
