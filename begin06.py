6b679a51e947bc150c11ccfc020ceacea9e754a7
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-20 10:58:21 +0500

    kod yozdim

--- begin06.py
+++ begin06.py
@@ -1,9 +1,5 @@
-# Assign 24 (int value) to 'a' variable;
-
-# Assign 3 (int value) to 'b' variable;
-
-# Assign 5 (int value) to 'c' variable;
-
-# Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin06';
-
-# # Print the variable 'k'.
+a=24
+b=3
+c=5
+k=a-b+c
+print (k)
